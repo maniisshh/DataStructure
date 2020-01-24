2670ceb9c5d7e4ef92c907296daba9d39f24fc74
Manish <manish20393@gmail.com>
2020-01-24 15:54:31 +0530

    hackerrank problems

---
+++ src/hackerrank/ReverseDoublyLinkedList.java
@@ -0,0 +1,97 @@
+package hackerrank;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ReverseDoublyLinkedList {
+
+    static class DoublyLinkedListNode {
+        public int data;
+        public DoublyLinkedListNode next;
+        public DoublyLinkedListNode prev;
+
+        public DoublyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    static class DoublyLinkedList {
+        public DoublyLinkedListNode head;
+        public DoublyLinkedListNode tail;
+
+        public DoublyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            DoublyLinkedListNode node = new DoublyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+                node.prev = this.tail;
+            }
+
+            this.tail = node;
+        }
+    }
+
+
+    public static void printDoublyLinkedList(DoublyLinkedListNode node, String sep) {
+        while (node != null) {
+            System.out.print(node.data + sep);
+            node = node.next;
+        }
+    }
+
+
+    static DoublyLinkedListNode reverse(DoublyLinkedListNode head) {
+        DoublyLinkedListNode prevNode = null;
+        DoublyLinkedListNode currentNode = head;
+        DoublyLinkedListNode nextNode = null;
+        while (currentNode != null) {
+            nextNode=currentNode.next;
+            currentNode.next=prevNode;
+            currentNode.prev=nextNode;
+            prevNode=currentNode;
+            currentNode=nextNode;
+        }
+
+        head=prevNode;
+
+        return head;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            DoublyLinkedList llist = new DoublyLinkedList();
+
+            int llistCount = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < llistCount; i++) {
+                int llistItem = scanner.nextInt();
+                scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+                llist.insertNode(llistItem);
+            }
+
+            DoublyLinkedListNode llist1 = reverse(llist.head);
+
+            printDoublyLinkedList(llist1, " ");
+
+        }
+
+
+        scanner.close();
+    }
+}
